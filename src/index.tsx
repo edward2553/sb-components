46e747edb7620a7daf97a9aeba1ab4ed9774fa48
Edward Andres <emoron@abatechnology.com>
2023-03-13 18:24:13 -0500

    feat: cahngo in node main.yml

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 import { MyLabel } from './components/MyLabel';
 import { Header } from './stories/Header';
 import { Page } from './stories/Page';
-export { Button } from './stories/Button';
+export { Button } from './stories/Button'; 
