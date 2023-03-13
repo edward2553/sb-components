87ddee13f6d96402fb4536e1dcc853c04a833d2f
Edward Andres <emoron@abatechnology.com>
2023-03-13 18:56:49 -0500

    fix: change inport for export on index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import { MyLabel } from './components/MyLabel';
-import { Header } from './stories/Header';
-import { Page } from './stories/Page';
-export { Button } from './stories/Button';   
+export { MyLabel } from './components/MyLabel';
+export { Header } from './stories/Header';
+export { Page } from './stories/Page';
+export { Button } from './stories/Button';     
